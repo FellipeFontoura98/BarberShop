96744d4ef777a1a49d521641105e3ee3d01dd08a
Fellipe Fontoura <56287188+FellipeFontoura98@users.noreply.github.com>
2024-03-05 12:08:08 -0300

    refecture: search bug

---
+++ app/(home)/_components/search.tsx
@@ -0,0 +1,12 @@
+import { Input } from "@/app/_components/ui/input";
+
+
+const Search = () => {
+    return ( 
+        <div className="flex items-center">
+            <Input />
+        </div>
+     );
+}
+ 
+export default Search
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,17 +5,17 @@ import { ptBR } from "date-fns/locale";
 export default function Home() {
   return (
     <div>
-    <Header />
+      <Header />
 
-    <div className="px-5 pt-5">
-    <h2 className="text-xl font-bold">Olá, Fellipe!</h2>
-    <p className="capitalize text-sm">
-      {format(new Date(), "EEEE',' dd 'de' MMMM", {
-      locale: ptBR,
-    })}
-    </p>
+      <div className="px-5 pt-5">
+       <h2 className="text-xl font-bold">Olá, Fellipe!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+           })}
+        </p>
+      </div>
     </div>
-  </div>
   )
 
 }
