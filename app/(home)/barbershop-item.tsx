28caff342dd08859e729033d5af8d01ed438d03d
Fellipe Fontoura <56287188+FellipeFontoura98@users.noreply.github.com>
2024-03-06 17:03:03 -0300

    fecture: erro

---
+++ app/(home)/barbershop-item.tsx
@@ -0,0 +1,19 @@
+import { Card, CardContent } from "../_components/ui/card";
+import { Barbershop } from '@prisma/client'
+
+interface BarbershopItemProps {
+    barbershop: Barbershop;
+}
+
+
+const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
+    return (
+        <Card>
+            <CardContent className="p-0">
+                <h1>{barbershop.name}</h1>
+            </CardContent>
+        </Card>
+    );
+}
+ 
+export default BarbershopItem;
