f244893248dead5560a1f79fd882a5080a3f406a
Fellipe Fontoura <56287188+FellipeFontoura98@users.noreply.github.com>
2024-03-11 20:23:26 -0300

    feat: ensure unautheticated user is redirected to login on booking click

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,9 +1,8 @@
-import { Button } from "@/app/_components/ui/button";
 import { db } from "@/app/_lib/prisma"
-import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
-import Image from "next/image";
 import BarbershopInfo from "./_components/barbershop-info";
 import ServiceItem from "./_components/services-item";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 
 interface BarbershopDetailsPageProps{
@@ -13,6 +12,7 @@ interface BarbershopDetailsPageProps{
 }
 
 const BarbershopDetailsPage = async ({params}: BarbershopDetailsPageProps) => {
+    const session = await getServerSession(authOptions)
     if (!params.id) {
         // TODO: redirecionar para home page
         return null;
@@ -38,7 +38,7 @@ const BarbershopDetailsPage = async ({params}: BarbershopDetailsPageProps) => {
 
             <div className="px-5 flex flex-col gap-4 py-6">
             {barbershop.Service.map((service) => (
-                <ServiceItem key={service.id} service={service} />
+                <ServiceItem key={service.id} service={service} isAuthenticated={!!session?.user} />
             ))}
             </div>
         </div>
