fa71f4baae2b07874d1ecbcdfa5869ac24f7cfd3
Fellipe Fontoura <56287188+FellipeFontoura98@users.noreply.github.com>
2024-03-21 11:07:20 -0300

    feat: fix error

--- app/(home)/barbershop-item.tsx
+++ app/(home)/barbershop-item.tsx
@@ -1,190 +1,55 @@
 "use client";
 
-import { Prisma } from "@prisma/client";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-import { Badge } from "./ui/badge";
-import { Card, CardContent } from "./ui/card";
-import { format, isFuture } from "date-fns";
-import { ptBR } from "date-fns/locale";
-import { Sheet, SheetClose, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { Badge } from "@/app/_components/ui/badge";
+import { Button } from "@/app/_components/ui/button";
+import { Card, CardContent } from "@/app/_components/ui/card";
+import { Barbershop } from "@prisma/client";
+import { StarIcon } from "lucide-react";
 import Image from "next/image";
-import { Button } from "./ui/button";
-import { cancelBooking } from "../_actions/cancel-booking";
-import { toast } from "sonner";
-import { useState } from "react";
-import { Loader2 } from "lucide-react";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "./ui/alert-dialog";
+import { useRouter } from "next/navigation";
 
-interface BookingItemProps {
-  booking: Prisma.BookingGetPayload<{
-    include: {
-      service: true;
-      barbershop: true;
-    };
-  }>;
+interface BarbershopItemProps {
+  barbershop: Barbershop;
 }
 
-const BookingItem = ({ booking }: BookingItemProps) => {
-  const [isDeleteLoading, setIsDeleteLoading] = useState(false);
+const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
+  const router = useRouter();
 
-  const isBookingConfirmed = isFuture(booking.date);
-
-  const handleCancelClick = async () => {
-    setIsDeleteLoading(true);
-
-    try {
-      await cancelBooking(booking.id);
-
-      toast.success("Reserva cancelada com sucesso!");
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsDeleteLoading(false);
-    }
+  const handleBookingClick = () => {
+    router.push(`/barbershops/${barbershop.id}`);
   };
 
   return (
-    <Sheet>
-      <SheetTrigger asChild>
-        <Card className="min-w-full">
-          <CardContent className="py-0 flex px-0">
-            <div className="flex flex-col gap-2 py-5 flex-[3] pl-5">
-              <Badge variant={isBookingConfirmed ? "default" : "secondary"} className="w-fit">
-                {isBookingConfirmed ? "Confirmado" : "Finalizado"}
-              </Badge>
-              <h2 className="font-bold">{booking.service.name}</h2>
-
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src={booking.barbershop.imageUrl} />
-
-                  <AvatarFallback>A</AvatarFallback>
-                </Avatar>
-
-                <h3 className="text-sm">{booking.barbershop.name}</h3>
-              </div>
-            </div>
-
-            <div className="flex flex-col items-center justify-center flex-1 border-l border-solid border-secondary">
-              <p className="text-sm capitalize">
-                {format(booking.date, "MMMM", {
-                  locale: ptBR,
-                })}
-              </p>
-              <p className="text-2xl">{format(booking.date, "dd")}</p>
-              <p className="text-sm">{format(booking.date, "hh:mm")}</p>
-            </div>
-          </CardContent>
-        </Card>
-      </SheetTrigger>
-
-      <SheetContent className="px-0">
-        <SheetHeader className="px-5 text-left pb-6 border-b border-solid border-secondary">
-          <SheetTitle>Informações da Reserva</SheetTitle>
-        </SheetHeader>
-
-        <div className="px-5">
-          <div className="relative h-[180px] w-full mt-6">
-            <Image src="/barbershop-map.png" fill alt={booking.barbershop.name} />
-
-            <div className="w-full absolute bottom-4 left-0 px-5">
-              <Card>
-                <CardContent className="p-3 flex gap-2">
-                  <Avatar>
-                    <AvatarImage src={booking.barbershop.imageUrl} />
-                  </Avatar>
-
-                  <div>
-                    <h2 className="font-bold">{booking.barbershop.name}</h2>
-                    <h3 className="text-xs overflow-hidden text-nowrap text-ellipsis">{booking.barbershop.address}</h3>
-                  </div>
-                </CardContent>
-              </Card>
-            </div>
+    <Card className="min-w-full max-w-full rounded-2xl">
+      <CardContent className="px-1 py-0 pt-1">
+        <div className="w-full h-[159px] relative">
+          <div className="absolute top-2 left-2 z-50">
+            <Badge variant="secondary" className="opacity-90 flex gap-1 items-center top-3 left-3">
+              <StarIcon size={12} className="fill-primary text-primary" />
+              <span className="text-xs">5,0</span>
+            </Badge>
           </div>
-
-          <Badge variant={isBookingConfirmed ? "default" : "secondary"} className="w-fit my-3">
-            {isBookingConfirmed ? "Confirmado" : "Finalizado"}
-          </Badge>
-
-          <Card>
-            <CardContent className="p-3 gap-3 flex flex-col">
-              <div className="flex justify-between">
-                <h2 className="font-bold">{booking.service.name}</h2>
-                <h3 className="font-bold text-sm">
-                  {" "}
-                  {Intl.NumberFormat("pt-BR", {
-                    style: "currency",
-                    currency: "BRL",
-                  }).format(Number(booking.service.price))}
-                </h3>
-              </div>
-
-              <div className="flex justify-between">
-                <h3 className="text-gray-400 text-sm">Data</h3>
-                <h4 className="text-sm">
-                  {format(booking.date, "dd 'de' MMMM", {
-                    locale: ptBR,
-                  })}
-                </h4>
-              </div>
-
-              <div className="flex justify-between">
-                <h3 className="text-gray-400 text-sm">Horário</h3>
-                <h4 className="text-sm">{format(booking.date, "hh:mm")}</h4>
-              </div>
-
-              <div className="flex justify-between">
-                <h3 className="text-gray-400 text-sm">Barbearia</h3>
-                <h4 className="text-sm">{booking.barbershop.name}</h4>
-              </div>
-            </CardContent>
-          </Card>
-
-          <SheetFooter className="flex-row gap-3 mt-6">
-            <SheetClose asChild>
-              <Button className="w-full" variant="secondary">
-                Voltar
-              </Button>
-            </SheetClose>
-
-            <AlertDialog>
-              <AlertDialogTrigger asChild>
-                <Button disabled={!isBookingConfirmed || isDeleteLoading} className="w-full" variant="destructive">
-                  Cancelar Reserva
-                </Button>
-              </AlertDialogTrigger>
-              <AlertDialogContent className="w-[90%]">
-                <AlertDialogHeader>
-                  <AlertDialogTitle>Deseja mesmo cancelar essa reserva?</AlertDialogTitle>
-                  <AlertDialogDescription>
-                    Uma vez cancelada, não será possível reverter essa ação.
-                  </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter className="flex-row gap-3">
-                  <AlertDialogCancel className="w-full mt-0">Voltar</AlertDialogCancel>
-                  <AlertDialogAction disabled={isDeleteLoading} className="w-full" onClick={handleCancelClick}>
-                    {isDeleteLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-                    Confirmar
-                  </AlertDialogAction>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
-          </SheetFooter>
+          <Image
+            alt={barbershop.name}
+            src={barbershop.imageUrl}
+            style={{
+              objectFit: "cover",
+            }}
+            fill
+            className="rounded-2xl"
+          />
         </div>
-      </SheetContent>
-    </Sheet>
+
+        <div className="px-2 pb-3">
+          <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">{barbershop.name}</h2>
+          <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">{barbershop.address}</p>
+          <Button className="w-full mt-3" variant="secondary" onClick={handleBookingClick}>
+            Reservar
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
   );
 };
 
-export default BookingItem;
+export default BarbershopItem;
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,14 +4,13 @@ import { ptBR } from "date-fns/locale";
 import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
 import { db } from "../_lib/prisma";
-
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import BarbershopItem from "./barbershop-item";
 
+
 export default async function Home() {
   const session = await getServerSession(authOptions);
-
   const [barbershops, confirmedBookings] = await Promise.all([
     db.barbershop.findMany({}),
     session?.user
@@ -29,24 +28,23 @@ export default async function Home() {
         })
       : Promise.resolve([]),
   ]);
-
   return (
     <div>
       <Header />
 
       <div className="px-5 pt-5">
-        <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+        <h2 className="text-xl font-bold">
+          {session?.user ? `Olá, ${session.user.name?.split(" ")[0]}!` : "Olá! Vamos agendar um corte hoje?"}
+        </h2>
         <p className="capitalize text-sm">
           {format(new Date(), "EEEE',' dd 'de' MMMM", {
             locale: ptBR,
           })}
         </p>
       </div>
-
       <div className="px-5 mt-6">
         <Search />
       </div>
-
       <div className="mt-6">
         {confirmedBookings.length > 0 && (
           <>
@@ -59,26 +57,21 @@ export default async function Home() {
           </>
         )}
       </div>
-
       <div className="mt-6">
         <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
-
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
       </div>
-
-      <div className="mt-6 mb-[4.5rem]">
-        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Populares</h2>
-
-        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+      <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
-            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            <div key={barbershop.id} className="min-w-[167px] max-w-[167px]">
+              <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            </div>
           ))}
         </div>
       </div>
-    </div>
   );
 }
